5825217045615785dfed24bb5e5d64521e13bb3c
Shawn Guerra-Bautista <shawnguerrabautista@gmail.com>
2021-09-26 13:40:54 -0400

    Implemented basic core components for the application

---
+++ src/components/skills/skills.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Skills = () => (
+    <div>
+        Skills
+    </div>
+);
+export default Skills;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,20 @@
-import * as React from "react"
+import React from "react"
 
+import Header from "../components/header/header"
+import About from "../components/about/about"
+import Projects from "../components/projects/projects"
+import Skills from "../components/skills/skills"
+import Footer from "../components/footer/footer"
 import { Layout } from "../layout/layout"
 
 const IndexPage = () => {
   return (
     <Layout>
+      <Header/>
+      <About/>
+      <Projects/>
+      <Skills/>
+      <Footer/>
     </Layout>
   )
 }
