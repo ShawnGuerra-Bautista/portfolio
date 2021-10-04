ee14518e9883101e7fdc697ab06c4eaef6644ef2
Shawn Guerra-Bautista <shawnguerrabautista@gmail.com>
2021-10-04 15:50:30 -0400

    added projects

--- src/mock/data.js
+++ src/mock/data.js
@@ -32,29 +32,92 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    title: 'Big Owl - Capstone Project',
+    date: 'Sep 2020 – Apr 2021',
+    info: 'An Android based app that automate the attendance of users at a given time and location similar to a clock-in clock-out system',
+    info2: 'Java, and Firebase',
+    url: 'https://github.com/Landry333/Big-Owl',
+    repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
     img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    title: 'Python Questions data set',
+    date: 'Mar 2020 – Apr 2020',
+    info: 'Created a document-oriented database, Couchbase (with SQL-like queries), using an existing data set, and then writing several queries for it.',
+    info2: 'Couchbase',
+    url: 'https://github.com/ShawnGuerra-Bautista/python_questions_couchbase_project',
+    repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
     img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    title: 'MovieLens SQL Queries',
+    date: 'Jan 2020 – Feb 2020',
+    info: 'A small project involving the creation of SQL queries to obtain the desired data from a rational database.',
+    info2: 'PostgreSQL',
+    url: 'https://github.com/ShawnGuerra-Bautista/Mini_SQL_Scripts_Project',
+    repo: '', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'project.jpg',
+    title: 'Mini Naïve Bayes Classifier',
+    date: 'Nov 2019',
+    info: 'A trained AI (on a small data set) which classifies whether a text has a negative or positive connotation using Naïve Bayes classifier',
+    info2: 'Python',
+    url: 'https://github.com/ShawnGuerra-Bautista/Word_Classifier_AI_Project/tree/Remastered/src',
+    repo: '', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'project.jpg',
+    title: 'X-rudder',
+    date: 'Oct 2019 – Nov 2019',
+    info: 'A 2-player game with an AI designed with a heuristic function and a Minimax algorithm.',
+    info2: 'Python',
+    url: 'https://github.com/ShawnGuerra-Bautista/x-rudder_game_AI_project/tree/master/src',
+    repo: '', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'project.jpg',
+    title: 'HTTP Client/Server',
+    date: 'Sep 2019 – Oct 2019',
+    info: 'A HTTP client and server software using TCP/UDP protocols to replicate the behaviour of HTTP requests/responses.',
+    info2: 'Java',
+    url: 'https://github.com/ShawnGuerra-Bautista/TCP-UDP_project',
+    repo: '', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'project.jpg',
+    title: 'K9Mail - Mini Capstone Project',
+    date: 'Jan 2019 – Apr 2019',
+    info: 'A mini capstone project that involves in update and create new features of an existing open source Android based application called K9Mail.',
+    info2: 'Java, and GreenDAO',
+    url: 'https://github.com/refatK/SOEN390-Project',
+    repo: '', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'project.jpg',
+    title: 'Library Catalog',
+    date: 'Aug 2018 – Dec 2018',
+    info: 'A website to manage inventory and users of a library catalog system',
+    info2: 'Angular, MySQL, and Spring (Java)',
+    url: 'https://github.com/kevin-tan/LibraryCatalog',
+    repo: '', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'project.jpg',
+    title: 'Huston Answers',
+    date: 'Jan 2018 – Apr 2018',
+    info: 'A Q&A website that allows users to ask questions (create a post) and give answers to them (reply to a post).',
+    info2: 'Angular, MySQL, and Spring (Java)',
+    url: 'https://github.com/kevin-tan/Q-AWebApplication',
+    repo: '', // if no repo, the button will not show up
   },
 ];
 
@@ -70,28 +133,18 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/shawnguerrab/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/ShawnGuerra-Bautista',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
